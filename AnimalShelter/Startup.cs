f6e5df9fd9c83b395c4f4f6e0d5c10bd46f2c054
Ben Russell <benjaminrussell36@gmail.com>
2020-08-21 15:30:20 -0700

    Add UseSwagger methods to Startup.cs

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 
 namespace AnimalShelter
@@ -25,7 +29,27 @@ namespace AnimalShelter
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "AnimalShelter API",
+                    Description = "An API to show dogs and cats",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ben Russell",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/brussell36"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +69,7 @@ namespace AnimalShelter
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                c.RoutePrefix = string.Empty;
             });
             // app.UseHttpsRedirection();
             app.UseMvc();
