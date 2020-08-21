2303715e22a81af8713cc21277531cb5eef9c7e0
Ben Russell <benjaminrussell36@gmail.com>
2020-08-21 12:19:51 -0700

    Add migrations for pagination

---
+++ AnimalShelter/Migrations/20200821191908_Pagination.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Pagination : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5,
+                column: "Story",
+                value: "She has always been that small and always will be.");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5,
+                column: "Story",
+                value: "She is has always been that small and always will be.");
+        }
+    }
+}
